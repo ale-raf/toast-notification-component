81ef2451dd5a8489b4d3d27075631d8e1e7b005e
Ale Raf <raffinalexis@gmail.com>
2024-04-08 11:15:14 +0200

    fichier liste des toasts

---
+++ toastsList.js
@@ -0,0 +1,22 @@
+export const toastsList = [
+  {
+    value: "Success",
+    color: "#22c55e",
+    active: false,
+  },
+  {
+    value: "Info",
+    color: "#3b82f6",
+    active: false,
+  },
+  {
+    value: "Warning",
+    color: "#f97316",
+    active: false,
+  },
+  {
+    value: "Error",
+    color: "#ef4444",
+    active: false,
+  },
+];
