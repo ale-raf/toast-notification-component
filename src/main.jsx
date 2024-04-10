b40f20a8b19ee8fd235f7d4ba66b7bfd3dc2a7c5
Ale Raf <raffinalexis@gmail.com>
2024-04-10 15:47:19 +0200

    ajout du provider pour le contexte des notifs

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import { App } from "./App.jsx";
+import { ToastPositionProvider } from "./contexts/toast-position.context.jsx";
 import "./style.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <ToastPositionProvider>
+      <App />
+    </ToastPositionProvider>
   </React.StrictMode>
 );
