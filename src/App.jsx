e6d0818daa1f40d980f33a8b0ebdcce2541b928f
Ale Raf <raffinalexis@gmail.com>
2024-04-10 16:07:36 +0200

    mise à jour globale fichier app

--- src/App.jsx
+++ src/App.jsx
@@ -1,127 +1,126 @@
 import { useState } from "react";
-import Button from "./Button";
-import Notification from "./Notification";
-import Form from "./Form";
+import { buttons } from "./data/buttons";
+import { Button } from "./Button";
+import { Toast } from "./Toast";
+import { Form } from "./Form";
+import { useToastPosition } from "./hooks/useToastPosition";
 
-function App() {
-  const toastsList = [
-    {
-      btnValue: "Success",
-      notifText: "Success toast notification",
-      notifIcon: "✅",
-      color: "#22c55e",
-      active: false,
-    },
-    {
-      btnValue: "Info",
-      notifText: "Info toast notification",
-      notifIcon: "ℹ️",
-      color: "#3b82f6",
-      active: false,
-    },
-    {
-      btnValue: "Warning",
-      notifText: "Warning toast notification",
-      notifIcon: "⚠️",
-      color: "#f97316",
-      active: false,
-    },
-    {
-      btnValue: "Error",
-      notifText: "Error toast notification",
-      notifIcon: "❌",
-      color: "#ef4444",
-      active: false,
-    },
-  ];
+export const App = () => {
+  const [toasts, setToasts] = useState([]);
 
-  const [toasts, setToasts] = useState([...toastsList]);
-  const [notificationsPosition, setNotificationsPosition] = useState({
-    position: "absolute",
-    top: "5px",
-    right: "15px",
-    positionClassName: "right",
-  });
+  const { toastPosition } = useToastPosition();
 
-  const handleChange = (e) => {
-    switch (e.target.id) {
-      case "top-left":
-        setNotificationsPosition({
-          position: "absolute",
-          left: "15px",
-          top: "5px",
-          positionClassName: "left",
-        });
+  // const [notification, setNotification] = useState({
+  //   message: "",
+  //   icon: "",
+  //   color: "",
+  //   active: false,
+  //   id: null,
+  // });
+
+  const getNotification = (event) => {
+    let notification = {};
+    switch (event.target.value) {
+      case "Success":
+        notification = {
+          message: "Success toast notification",
+          active: false,
+          color: "#22c55e",
+          icon: "✅",
+          id: Date.now(),
+        };
         break;
-      case "bottom-left":
-        setNotificationsPosition({
-          position: "absolute",
-          left: "15px",
-          bottom: "5px",
-          positionClassName: "left",
-        });
+      case "Info":
+        notification = {
+          message: "Info toast notification",
+          active: false,
+          color: "#3b82f6",
+          icon: "ℹ️",
+          id: Date.now(),
+        };
         break;
-      case "bottom-right":
-        setNotificationsPosition({
-          position: "absolute",
-          right: "15px",
-          bottom: "5px",
-          positionClassName: "right",
-        });
+      case "Warning":
+        notification = {
+          message: "Warning toast notification",
+          active: false,
+          color: "#f97316",
+          icon: "⚠️",
+          id: Date.now(),
+        };
         break;
-      default:
-        setNotificationsPosition({
-          position: "absolute",
-          right: "15px",
-          top: "5px",
-          positionClassName: "right",
-        });
+      case "Error":
+        notification = {
+          message: "Error toast notification",
+          active: false,
+          color: "#ef4444",
+          icon: "❌",
+          id: Date.now(),
+        };
         break;
     }
+    return notification;
   };
 
-  const handleClick = (e, index) => {
-    let newToasts = [...toasts];
-    newToasts[index].active = e.target.className === "toast-btn" ? true : false;
+  const handleClick = (e) => {
+    let myNotif = getNotification(e);
+    // let newNotif = {
+    //   ...notification,
+    //   message: myNotif.message,
+    //   icon: myNotif.icon,
+    //   color: myNotif.color,
+    //   active: true,
+    // };
+    // setNotification(myNotif);
+    const newToasts = [...toasts, myNotif];
+    let myIndex = newToasts.indexOf(myNotif);
+    newToasts[myIndex].active = true;
     setToasts(newToasts);
-    // notifications loose active status after 5 seconds
+
+    // notifications loose active status after 4 seconds
     setTimeout(() => {
-      newToasts = [...toasts];
-      newToasts[index].active = false;
+      //   newNotif = { ...notification, active: false };
+      //   setNotification(newNotif);
+      // let filteredToasts = newToasts.filter((toast) => toast.active === false);
+      // newToasts = [...toasts, myNotif];
+      newToasts[myIndex].active = false;
       setToasts(newToasts);
     }, 4000);
+  };
+
+  const closeToast = (index) => {
+    const myToasts = [...toasts];
+    myToasts[index].active = false;
+    setToasts(myToasts);
   };
 
   return (
     <div className="app">
       <h1>Toast Notification Component</h1>
       <h3>Customize the position of the notifications :</h3>
-      <Form onChange={(e) => handleChange(e)} />
-      {toasts.map((toast, index) => (
+      <Form />
+      {buttons.map((button, index) => (
         <Button
-          key={toast.btnValue}
-          value={toast.btnValue}
-          bgColor={toast.color}
+          key={button.value}
+          value={button.value}
+          bgColor={button.color}
           onClick={(e) => handleClick(e, index)}
         />
       ))}
-      <div style={notificationsPosition}>
-        {toasts.map((toast, index) =>
-          toast.active ? (
-            <Notification
-              key={toast.notifText}
-              icon={toast.notifIcon}
-              text={toast.notifText}
-              bgColor={toast.color}
-              activeClassName={toast.active ? "notification-active" : null}
-              positionClassName={notificationsPosition.positionClassName}
-              onClick={(e) => handleClick(e, index)}
-            />
-          ) : null
+      <div style={toastPosition}>
+        {toasts.map(
+          (toast, index) =>
+            toast.active && (
+              <Toast
+                key={index}
+                icon={toast.icon}
+                message={toast.message}
+                bgColor={toast.color}
+                activeClassName={toast.active ? "notification-active" : ""}
+                onClick={() => closeToast(index)}
+              />
+            )
         )}
       </div>
     </div>
   );
-}
-
-export default App;
+};
