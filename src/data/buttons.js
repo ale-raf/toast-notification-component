62ba08e5ea675d7f34d3a036a4a80b3b06ed0050
Ale Raf <raffinalexis@gmail.com>
2024-04-10 15:44:41 +0200

    données liées aux boutons

---
+++ src/data/buttons.js
@@ -0,0 +1,18 @@
+export const buttons = [
+  {
+    value: "Success",
+    color: "#22c55e",
+  },
+  {
+    value: "Info",
+    color: "#3b82f6",
+  },
+  {
+    value: "Warning",
+    color: "#f97316",
+  },
+  {
+    value: "Error",
+    color: "#ef4444",
+  },
+];
